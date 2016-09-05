4eba9157222e902a185f44c512d4ef6d01ba15da
Jørgen Skogmo <js@fndn.dk>
2016-09-05 14:09:24 +0200

    check user

--- up.sh
+++ up.sh
@@ -7,7 +7,26 @@ DATE=$(date +%y%m%d\ %H:%M)
 GREEN="\033[1;32m"
 ENDCOLOR="\033[0m"
 
+MESSAGE="$DATE update"
 
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -m|--message)
+    MESSAGE="$2"
+    shift # past argument
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+echo "message: ${MESSAGE}"
+#exit
 
 # copy everything in build to _site
 rm -rf _site/*
@@ -16,12 +35,12 @@ cp -v -R build/* _site/
 # commit and push gh-pages branch
 cd _site
 git add .
-git commit -m "$DATE update"
+git commit -m "${MESSAGE}"
 git push origin gh-pages
 
 # commit and push master
 cd ../
-git commit -a -m "$DATE update"
+git commit -a -m "${MESSAGE}"
 git push origin master
 
 git log --stat -1
