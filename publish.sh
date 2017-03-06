14d384d061802d1c03bcd24fa679ec8ea8226ae6
Jørgen Skogmo <js@base.io>
2017-03-06 19:36:09 +0100

    170306 19:36 update

---
+++ publish.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Use this if you do NOT want to commit code-changes (just content)
+
+
+DATE=$(date +%y%m%d\ %H:%M)
+GREEN="\033[1;32m"
+ENDCOLOR="\033[0m"
+
+MESSAGE="$DATE content-update"
+
+echo "message: ${MESSAGE}"
+
+# copy everything in build to docs (github serves the site from the /docs directory)
+rm -rf docs/*
+cp -v -R build/* docs/
+
+# commit and push content repo
+cd ../content/
+git add .
+git commit -m "${MESSAGE}"
+git push origin master
+
+# commit and push master
+cd ../site/
+#git add .
+#git commit -a -m "${MESSAGE}"
+git commit -m 'message' -- docs
+git push origin master
+
+#git log --stat -1
+
+echo $GREEN"Published as '$MESSAGE'" $ENDCOLOR
+
+npm version patch
